6fad9b2dd66d8e20e774afd6047c9487c65bce1e
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2022-10-14 14:17:07 +0100

    Update

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace cf_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string players = Console.ReadLine();
+            string test = "123";
+            int i = 1;
+            int count = 1;
+            int maxCount = 1;  
+            char cp = players[0];  
+            int l = players.Length;  
+            while ((i < l) && (maxCount < 7))  {
+            if (cp == players[i]){
+                count++;
+                if (count > maxCount){
+                maxCount = count;
+                }
+            } else {
+                cp = players[i];
+                count = 1;
+            }
+            i++;
+            }
+            if (maxCount > 6) {
+                Console.WriteLine("YES");
+            } else {
+                Console.WriteLine("NO");
+            }
+
+
+        }
+    }
+}
